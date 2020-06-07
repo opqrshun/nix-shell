0499f4dc00d1300739737c3c544d3cadf66328a2
ttaki <opqrshun@gmail.com>
2020-06-07 22:09:12 +0900

    Create cmd.nix

---
+++ cmd.nix
@@ -0,0 +1,50 @@
+with (import <nixpkgs> {});
+
+let
+
+  packages=with pkgs; [
+    bat
+    broot
+    cloc
+    exa
+    fd
+    fzf
+    git
+    htop
+    neofetch
+    nkf
+    neovim
+    nmap
+    procs
+    ranger
+    ripgrep
+    tmux
+    tig
+    tldr
+    zsh
+  ];
+
+  language=with pkgs; [
+  ];
+
+  devtools=with pkgs; [
+    shellcheck
+    lazygit
+    lazydocker
+  ];
+
+in
+
+pkgs.mkShell {
+  buildInputs = [
+    packages
+    language
+    devtools
+  ];
+
+  shellHook =
+    ''
+      echo "Hello shell"
+      zsh
+    '';
+}
